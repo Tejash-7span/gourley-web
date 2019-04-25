1501e6b16f8e66641a07aa6af6726ad739852611
Jaimin Panchal <jaiminpanchal@Jaimins-MacBook-Air.local>
2019-04-25 10:54:21 +0530

    Implement rest.service.ts

--- src/app/general/services/rest.service.ts
+++ src/app/general/services/rest.service.ts
@@ -5,99 +5,99 @@ import { PagedData } from '../models/paged-data.model';
 import { Router } from '@angular/router';
 import { MessageService } from './message.service';
 import { PER_PAGE } from '../models/constants';
-
+import { environment } from '../../../environments/environment';
 @Injectable()
 export class RestService {
-    private apiURL = 'http://localhost:5000';
+    private apiURL = environment.serviceURL;
 
-    constructor(private httpClient: HttpClient,
-        private localStorageService: LocalStorageService,
-        private messageService: MessageService) {
+        constructor(private httpClient: HttpClient,
+            private localStorageService: LocalStorageService,
+            private messageService: MessageService) {
 
-    }
+}
 
     private get requestHeaders(): any {
-        const headers = {
-            'Content-Type': 'application/json',
-        };
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
-        if (this.localStorageService.token) {
-            headers['Authorization'] = `bearer ${this.localStorageService.token}`;
-        }
-        return headers;
+    if (this.localStorageService.token) {
+        headers['Authorization'] = `bearer ${this.localStorageService.token}`;
     }
+    return headers;
+}
 
-    public getPagedData<T>(path: string, page: number, searchTerm: string = ''): Promise<PagedData<T>> {
-        const url = `${this.apiURL}/${path}?page=${page}&perpage=${PER_PAGE}&searchterm=${searchTerm}`;
-        return new Promise<PagedData<T>>((resolve, reject) => {
-            this.httpClient.get<T[]>(url, { headers: this.requestHeaders, observe: 'response' })
-                .toPromise().then(response => {
-                    let totalRecords = 0;
-                    if (response.headers.has('TotalRecords')) {
-                        totalRecords = +response.headers.get('TotalRecords');
-                    }
-                    resolve(new PagedData<T>(response.body, page, totalRecords));
-                }).catch(rejected => {
-                    reject(this.handleRejected(rejected));
-                });
-        });
-    }
+    public getPagedData<T>(path: string, page: number, searchTerm: string = ''): Promise < PagedData < T >> {
+    const url = `${this.apiURL}/${path}?page=${page}&perpage=${PER_PAGE}&searchterm=${searchTerm}`;
+    return new Promise<PagedData<T>>((resolve, reject) => {
+        this.httpClient.get<T[]>(url, { headers: this.requestHeaders, observe: 'response' })
+            .toPromise().then(response => {
+                let totalRecords = 0;
+                if (response.headers.has('TotalRecords')) {
+                    totalRecords = +response.headers.get('TotalRecords');
+                }
+                resolve(new PagedData<T>(response.body, page, totalRecords));
+            }).catch(rejected => {
+                reject(this.handleRejected(rejected));
+            });
+    });
+}
 
-    public get<T>(path: string): Promise<T> {
-        const url = `${this.apiURL}/${path}`;
-        return new Promise<T>((resolve, reject) => {
-            this.httpClient.get<T>(url, { headers: this.requestHeaders })
-                .toPromise().then(response => {
-                    resolve(response);
-                }).catch(rejected => {
-                    reject(this.handleRejected(rejected));
-                });
-        });
-    }
+    public get<T>(path: string): Promise < T > {
+    const url = `${this.apiURL}/${path}`;
+    return new Promise<T>((resolve, reject) => {
+        this.httpClient.get<T>(url, { headers: this.requestHeaders })
+            .toPromise().then(response => {
+                resolve(response);
+            }).catch(rejected => {
+                reject(this.handleRejected(rejected));
+            });
+    });
+}
 
-    public post<T>(path: string, data: any): Promise<T> {
-        const url = `${this.apiURL}/${path}`;
-        return new Promise<T>((resolve, reject) => {
-            this.httpClient.post<T>(url, data, { headers: this.requestHeaders })
-                .toPromise().then(response => {
-                    resolve(response);
-                }).catch(rejected => {
-                    reject(this.handleRejected(rejected));
-                });
-        });
-    }
+    public post<T>(path: string, data: any): Promise < T > {
+    const url = `${this.apiURL}/${path}`;
+    return new Promise<T>((resolve, reject) => {
+        this.httpClient.post<T>(url, data, { headers: this.requestHeaders })
+            .toPromise().then(response => {
+                resolve(response);
+            }).catch(rejected => {
+                reject(this.handleRejected(rejected));
+            });
+    });
+}
 
-    public put<T>(path: string, data: any): Promise<T> {
-        const url = `${this.apiURL}/${path}`;
-        return new Promise<T>((resolve, reject) => {
-            this.httpClient.put<T>(url, data, { headers: this.requestHeaders })
-                .toPromise().then(response => {
-                    resolve(response);
-                }).catch(rejected => {
-                    reject(this.handleRejected(rejected));
-                });
-        });
-    }
+    public put<T>(path: string, data: any): Promise < T > {
+    const url = `${this.apiURL}/${path}`;
+    return new Promise<T>((resolve, reject) => {
+        this.httpClient.put<T>(url, data, { headers: this.requestHeaders })
+            .toPromise().then(response => {
+                resolve(response);
+            }).catch(rejected => {
+                reject(this.handleRejected(rejected));
+            });
+    });
+}
 
-    public delete<T>(path: string): Promise<T> {
-        const url = `${this.apiURL}/${path}`;
-        return new Promise<T>((resolve, reject) => {
-            this.httpClient.delete<T>(url, { headers: this.requestHeaders })
-                .toPromise().then(response => {
-                    resolve(response);
-                }).catch(rejected => {
-                    reject(this.handleRejected(rejected));
-                });
-        });
-    }
+    public delete <T>(path: string): Promise < T > {
+    const url = `${this.apiURL}/${path}`;
+    return new Promise<T>((resolve, reject) => {
+        this.httpClient.delete<T>(url, { headers: this.requestHeaders })
+            .toPromise().then(response => {
+                resolve(response);
+            }).catch(rejected => {
+                reject(this.handleRejected(rejected));
+            });
+    });
+}
 
     private handleRejected(rejected: any): RejectedResponse {
-        if (rejected.status && rejected.status === 401) {
-            this.messageService.sendLogoutMessage();
-        }
-
-        return new RejectedResponse(rejected);
+    if (rejected.status && rejected.status === 401) {
+        this.messageService.sendLogoutMessage();
     }
+
+    return new RejectedResponse(rejected);
+}
 }
 
 export class RejectedResponse {
